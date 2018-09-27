29f79061628bea86712826fbec35465a83d34dfd
Bastien <bastien.van-delft@unchartech.com>
2018-09-27 17:25:55 +0200

    curl - L

--- QANet-pytorch/download.sh
+++ QANet-pytorch/download.sh
@@ -3,13 +3,13 @@
 # Download SQuAD
 SQUAD_DIR=./data/squad
 mkdir -p $SQUAD_DIR
-curl https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json -O $SQUAD_DIR/train-v1.1.json
-curl https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json -O $SQUAD_DIR/dev-v1.1.json
+curl -L https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json -O $SQUAD_DIR/train-v1.1.json
+curl -L https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json -O $SQUAD_DIR/dev-v1.1.json
 
 # Download GloVe
 GLOVE_DIR=./data/glove
 mkdir -p $GLOVE_DIR
-curl http://nlp.stanford.edu/data/glove.840B.300d.zip -O $GLOVE_DIR/glove.840B.300d.zip
+curl -L http://nlp.stanford.edu/data/glove.840B.300d.zip -O $GLOVE_DIR/glove.840B.300d.zip
 unzip $GLOVE_DIR/glove.840B.300d.zip -d $GLOVE_DIR
 #wget http://nlp.stanford.edu/data/glove.6B.zip -O $GLOVE_DIR/glove.6B.zip
 #unzip $GLOVE_DIR/glove.6B.zip -d $GLOVE_DIR
